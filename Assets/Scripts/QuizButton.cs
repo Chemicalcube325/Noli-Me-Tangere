6f9493e4e2b0425c6aa55162f2490c87586cec22
Jivan-Plata <jivanplata@gmail.com>
2024-05-18 15:07:02 +0800

    Initial code

---
+++ Assets/Scripts/QuizButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizButton : MonoBehaviour
+{
+
+    public void CorrectAnswer()
+    {
+        Debug.Log("Correct Answer: Button Clicked");
+        // Nothing Happens
+    }
+    public void WrongAnswer()
+    {
+        Debug.Log("Wrong Answer: Button Clicked");
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
